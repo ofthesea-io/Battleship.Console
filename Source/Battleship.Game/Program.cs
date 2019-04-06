6f534254363b0dd32361959f0192391c922d1b8d
Jakes Potgieter <visualsanity@hotmail.com>
2019-04-06 11:16:18 +0100

    Retrofit - Sync

--- Source/Battleship.Game/Program.cs
+++ Source/Battleship.Game/Program.cs
@@ -49,8 +49,8 @@ namespace Battleship.Game
 
             if (args.Length == 0)
             {
-                //p.SimulateGamePlay("{'SimulationTimer': 0, 'X':['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],'Y':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]}");
-                p.Run();
+                p.SimulateGamePlay("{'SimulationTimer': 0, 'X':['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],'Y':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]}");
+                //p.Run();
             }
             else
             {
